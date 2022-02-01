59c40ae1ab0bab6b9217b31c45dc9a91795fbf77
Anurag000-rgb <22anurag44@gmail.com>
2022-02-01 06:32:13 +0530

    All

---
+++ src/Components/Animations/TextAnimation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+
+
+export default function TextAnimation (){
+    
+    const letterArray = "Rarestate".split("");
+
+    return (
+        <Wrapper>
+            {letterArray.map((item, index)=> (
+                <span key={index}>{item}</span>
+            ))}
+        </Wrapper>
+    )
+}
+
+
+
+const animation = keyframes`
+  0% { opacity: 0; transform: translateY(-100px) skewX(10deg) skewY(10deg) rotateZ(30deg); filter:
+     blur(10px); }
+  25% { opacity: 1; transform: translateY(0px) skewX(0deg) skewY(0deg) rotateZ(0deg); filter:
+     blur(0px); }
+  75% { opacity: 1; transform: translateY(0px) skewX(0deg) skewY(0deg) rotateZ(0deg); filter:
+     blur(0px); }
+  100% { opacity: 0; transform: translateY(-100px) skewX(10deg) skewY(10deg) rotateZ(30deg); filter:
+     blur(10px); }
+`;
+const Wrapper = styled.span`
+        display: inline-block;
+
+    color: purple;
+    
+    span{
+        display: inline-block;
+
+        opacity: 0;
+    animation-name: ${animation};
+    animation-duration: 6s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    }
+`;
---
+++ src/Components/Home.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+//import backgroundImg from '../public/image/img-pro-2.png';
+import TextAnimation from './Animations/TextAnimation'
+//    background-image: url("/image/metaverse101.jpeg");
+import Footer from './Footer';
+
+//           <Link to="/Marketplace/Entry">
+//          </Link>
+
+
+export default function Home() {
+  return (
+    
+    <Container>
+      <Content>
+        <CTA>
+        
+          <Description>Welcome To <TextAnimation/> Metaverse</Description>
+          <Line>A Marketplace For Real Estate 🏡  & 🏘️</Line>
+          <Line>Based On Land Minted NFTs Only available on Rarestate</Line>
+          <SignUp href="https://www.google.com/">Launch Marketplace</SignUp>
+          <SignUp_1>Read More</SignUp_1>
+        </CTA>
+        <BgImage/>
+        
+      </Content>
+      <Footer/>
+    </Container>
+  )
+};
+
+
+
+
+
+
+const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 60px;
+    background-color: #040714;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: -1;
+`;
+const Container = styled.section`
+    
+    flex-direction: column;
+    overflow: hidden;
+    display: flex;
+    text-align: center;
+    height: 100vh;
+    position: relative;
+`;
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("images/metaverse.jpeg");
+    top: 100;
+    right: 0;
+    left: 0;
+    position: absolute;
+    z-index: -100;
+`;
+const Bg_Image = styled.div`
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/image/metaverse101.jpeg");
+    top: 0;
+    right: 0;
+    left: 0;
+    position: absolute;
+    z-index: -1;
+`;
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+`;
+const CTA = styled.div`
+    margin-bottom: 2vw;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center;
+    margin-right: auto;
+    margin-left: auto;
+    transition-timing-function: ease-out;
+    transition: opacity 0.2s;
+    width: 100%;
+`;
+
+const Description = styled.p`
+    color: white;
+    background: linear-gradient(180deg, #730040 0%, #301CBE 100%);
+  
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    
+    font-size: 6rem;
+    
+    margin: 0 0 15px;
+    line-height: 1.5;
+    font-weight: 600;
+    letter-spacing: 0.025em;
+    
+
+
+`;
+const Line = styled.p`
+  color: black;
+    font-size: 20px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1px;
+
+`;
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: black;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transprent;
+    border-radius: 4px;
+    &:hover {
+      color: black;
+        background-color: white;
+        cursor: pointer;
+        border: 1px solid black;
+    }
+`;
+const SignUp_1 = styled.a`
+    font-weight: bold;
+    color: black;
+    border: 0.25px solid black;
+    background-color: #f9f9f9;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transprent;
+    border-radius: 4px;
+    &:hover {
+      color: white;
+        background-color: black;
+        cursor: pointer;
+    }
+`;
+
+// #0063e5 #0483ee
+//    color: hsla(0, 0%, 95.3%, 1);
+
